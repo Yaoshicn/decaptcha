c3cabde830ddf0d39b3fc487c33456605235597a
yaoshi <guchaojiecn@foxmail.com>
2015-10-01 22:14:12 +0800

    update readme adn add demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # decaptcha
  通过简单的图像识别算法来完成验证码识别
-## 图像处理
+## 前期准备
 ### Image(图像处理库)
 - jpeg http://www.ijg.org/files/
 - zlib http://www.zlib.net
@@ -9,8 +9,16 @@
 ### numpy(数学处理库)
 
 ### ImageEnhance(图像处理库)
+## 图像处理
+
+### 1.图片清除噪点
+### 2.图片清除干扰线
+### 3.图片切割
+### 4.信息输出
 ## 验证码识别
 ### KNN(K邻近算法)
 
 ### SVM(支持向量机)
+- http://www.csie.ntu.edu.tw/~cjlin/libsvm/index.html?js=1#svm-toy-js
+- http://www.pami.sjtu.edu.cn/people/gpliu/document/libsvm_src.pdf
 
--- test.py
+++ test.py
@@ -1,8 +1,61 @@
 __author__ = 'Yaoshi'
-from PIL import Image
+from PIL import Image,ImageDraw,ImageEnhance
 
-def openImg():
-    im = Image.open('1.png')
-    im.show()
+def getPixel(image,x,y,G,N):
+    L = image.getpixel((x,y))
+    if L > G:
+        L = True
+    else:
+        L = False
 
-openImg()
+    nearDots = 0
+    if L == (image.getpixel((x - 1,y - 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x - 1,y)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x - 1,y + 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x,y - 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x,y + 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x + 1,y - 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x + 1,y)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x + 1,y + 1)) > G):
+        nearDots += 1
+
+    if nearDots < N:
+        return image.getpixel((x,y-1))
+    else:
+        return None
+
+def clearNoise(image,G,N,Z):
+    draw = ImageDraw.Draw(image)
+
+    for i in xrange(0,Z):
+        for x in xrange(1,image.size[0] - 1):
+            for y in xrange(1,image.size[1] - 1):
+                color = getPixel(image,x,y,G,N)
+                if color != None:
+                    draw.point((x,y),color)
+
+def enhanceImage(image):
+    enhancedImage = ImageEnhance.Sharpness(image).enhance(3)
+    return enhancedImage
+
+def main():
+    image = Image.open("duowei.png")
+
+    enhanceImage(image)
+
+    image = image.convert("L")
+
+    clearNoise(image,50,4,4)
+
+    image.save("result.jpg")
+
+if __name__ == '__main__':
+    main()
+
